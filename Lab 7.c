f63441cd4ce55fe9c7f2e3c1183aae7a44ab7786
md asif molla <105847168+mollaasif@users.noreply.github.com>
2022-05-21 13:43:09 +0600

    Add files via upload

---
+++ Lab 7.c
@@ -0,0 +1,26 @@
+
+#include<stdio.h>
+int main()
+{
+
+int number,sum, a,b,c,d,e, d1,d2,d3,d4,d5;
+printf("Enter the 5 digit number: ");
+scanf("%d",&number);
+
+a=number/10;
+d1=number%10;
+b=a/10;
+d2=a%10;
+c=b/10;
+d3=b%10;
+d=c/10;
+d4=c%10;
+e=d/10;
+d5=d%10;
+sum=d1+d2+d3+d4+d5;
+printf("reverse order: ");
+printf("sum : %d\n",sum);
+
+return 0;
+
+}
